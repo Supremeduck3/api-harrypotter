a07f4c19895f5fd4637f718fbce40b5ac3cdd06f
Supremeduck008 <pedro.otaviobraga@gmail.com>
2025-08-25 14:55:27 -0300

    Att

--- server.js
+++ server.js
@@ -1,36 +1,71 @@
 import express from "express";
-import bruxos from "./src/data/bruxos.js";
+import dados from "./src/data/dados.js";
+const {varinhas} = dados;
+const {animais} = dados;
+const {pocoes} = dados;
 
 const app = express();
 
-const serverport = 3000
+const serverport = 3002
 
 app.get("/",(req,res) => {
-    res.send("Minha API de harry potter está ativa")
-})
-app.get("/bruxos", (req, res) => {
-    res.json(bruxos)
-})
-//------------------------------------------------------------------------------
-app.get("/bruxos/:id", (req, res) => {
-    let id = parseInt(req.params.id)
-    const bruxo = bruxos.find (b => b.id === id);
-    if (bruxo) {
-        res.json({
-            success: true,
-            message: `Bruxo ${bruxo.nome} encontrado! ⚡`,
-            data: bruxo
-        });
-    } else {
+    res.send("Minha API de harry potter está ativa");
+});
+
+app.get("/varinhas", (req, res) => {
+    res.json(varinhas);
+});
+app.get("/animais", (req, res) => {
+    res.json(animais);
+});
+app.get("/pocoes", (req, res) => {
+    res.json(pocoes);
+});
+
+app.get("/varinhas/:id", (req, res)=>{
+    let id = req.params.id;
+    id = parseInt(id);
+
+    const varinha = varinhas.find(b => b.id === id);
+
+    if(varinha){
+        res.status(200).json(varinha)
+    }else{
         res.status(404).json({
-            success: false,
-            error: "Bruxo não encontrado ",
-            message: `Nenhum bruxo com ID ${id} foi encontrado`,
-            codigo: "WIZARD_NOT_FOUND"
+            erro: "Essa varinha não foi encontrada"
+        });
+    }
+});
+app.get("/animais/:id", (req, res)=>{
+    let id = req.params.id;
+    id = parseInt(id);
+
+    const animal = animais.find(b => b.id === id);
+
+    if(animal){
+        res.status(200).json(animal)
+    }else{
+        res.status(404).json({
+            erro: "Esse animal não foi encontrado"
+        });
+    }
+});
+app.get("/pocoes/:id", (req, res)=>{
+    let id = req.params.id;
+    id = parseInt(id);
+
+    const pocao = pocoes.find(b => b.id === id);
+
+    if(pocao){
+        res.status(200).json(pocao)
+    }else{
+        res.status(404).json({
+            erro: "Esse animal não foi encontrado"
         });
     }
 });
 
+
 app.listen(serverport, () => {
     console.log(`Meu servidor está funcionando na porta http://localhost:${serverport}`)
-})
+});
---
+++ src/data/dados.js
@@ -0,0 +1,65 @@
+
+const varinhas = [
+    {
+        id: 1,
+        varinha: "Azevinho e pena de fênix, 28cm",
+    },
+    {
+        id: 2,
+        varinha: "Videira e fibra de coração de dragão, 27cm",
+    },
+    {
+        id: 3,
+        varinha: "Salgueiro e crina de unicórnio, 35cm",
+    },
+    {
+        id: 4,
+        varinha: "Espinheiro e crina de unicórnio, 25cm",
+    },
+]
+const animais = [
+    {
+        id: 1,
+        animais: "Azevinho e pena de fênix, 28cm",
+        tipo:"Correio/Companhia"
+    },
+    {
+        id: 2,
+        animais: "Gato",
+        tipo:"Companhia"
+    },
+    {
+        id: 3,
+        animais: "Sapo",
+        tipo:"Companhia"
+    },
+    {
+        id: 4,
+        animais: "Rato",
+        tipo:"Companhia"
+    },
+]
+const pocoes = [
+    {
+        id:1,
+        pocao:"Polissuco",
+        efeito:"Transforma na aparência de outra pessoa"
+    },
+    {
+        id:2,
+        pocao:"Felix Felicis",
+        efeito:"Sorte temporária"
+    },
+    {
+        id:3,
+        pocao:"Poção do amor",
+        efeito:"Transforma na aparência de outra pessoa"
+    },
+    {
+        id:4,
+        pocao:"Veritaserum",
+        efeito:"Força a dizer a verdade"
+    }
+]
+
+export default {varinhas, animais, pocoes}
